cf2a70229a211408350507b56d70e97fde20fddf
Elizabeth Polgreen <epolgreen@gmail.com>
2021-05-21 20:19:54 +0100

    use long

--- PBE/make_PBE_reffuncs.sh
+++ PBE/make_PBE_reffuncs.sh
@@ -1,4 +1,4 @@
-for f in *.sl; do
+for f in *0.sl; do
 echo "#include <assert.h> 
 #include <iostream>
 #include <sstream>
@@ -15,7 +15,7 @@ echo "int main(int argc, const char *argv[])
 		return 1;
 	}
 
-	unsigned int a;
+	unsigned long int a;
 
 	// arg 1 is x
 	std::istringstream ssA(argv[1]);
@@ -29,18 +29,18 @@ echo "int main(int argc, const char *argv[])
 g++ $f-ref.cpp -o $f-ref
 
 
-echo "(set-logic BV)
-(synth-fun f ((x (_ BitVec 64))) (_ BitVec 64)
-    ((Start (_ BitVec 64)))
-    ((Start (_ BitVec 64) (#x0000000000000000 #x0000000000000001 x (bvnot Start) (bvshl Start #x0000000000000001) (bvlshr Start #x0000000000000001) (bvlshr Start #x0000000000000004) (bvlshr Start #x0000000000000010) (bvand Start Start) (bvor Start Start) (bvxor Start Start) (bvadd Start Start) (ite (= Start #x0000000000000001) Start Start)))))" > $f.new.sl
+# echo "(set-logic BV)
+# (synth-fun f ((x (_ BitVec 64))) (_ BitVec 64)
+#     ((Start (_ BitVec 64)))
+#     ((Start (_ BitVec 64) (#x0000000000000000 #x0000000000000001 x (bvnot Start) (bvshl Start #x0000000000000001) (bvlshr Start #x0000000000000001) (bvlshr Start #x0000000000000004) (bvlshr Start #x0000000000000010) (bvand Start Start) (bvor Start Start) (bvxor Start Start) (bvadd Start Start) (ite (= Start #x0000000000000001) Start Start)))))" > $f.new.sl
 
-echo "(declare-var x (_ BitVec 64))
-(declare-oracle-fun rand rand ((_ BitVec 64)) (_ BitVec 64))
-(declare-oracle-fun reffunction">> $f.new.sl 
-echo $f-ref >> $f.new.sl 
-echo "((_ BitVec 64)) (_ BitVec 64))
-(constraint (=> (and (< (_ bv0 64) x)(>= (_ bv10 64) x))(= (f(rand x)) (reffunction (rand x)))))
-(check-synth)  " >> $f.new.sl
+# echo "(declare-var x (_ BitVec 64))
+# (declare-oracle-fun rand rand ((_ BitVec 64)) (_ BitVec 64))
+# (declare-oracle-fun reffunction">> $f.new.sl 
+# echo $f-ref >> $f.new.sl 
+# echo "((_ BitVec 64)) (_ BitVec 64))
+# (constraint (=> (and (< (_ bv0 64) x)(>= (_ bv10 64) x))(= (f(rand x)) (reffunction (rand x)))))
+# (check-synth)  " >> $f.new.sl
 
 done
 
