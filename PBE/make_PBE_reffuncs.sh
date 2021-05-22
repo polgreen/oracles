1b6089ad142187f6a0bdeb44072190a5b778e98d
Elizabeth Polgreen <epolgreen@gmail.com>
2021-05-22 17:17:33 +0100

    only build existing .cpp files

--- PBE/make_PBE_reffuncs.sh
+++ PBE/make_PBE_reffuncs.sh
@@ -1,31 +1,31 @@
 for f in *0.sl; do
-echo "#include <assert.h> 
-#include <iostream>
-#include <sstream>
-#include <stdlib.h> 
-#define WIDTH 64" > $f-ref.cpp
-smt2c "$(tail -n +2 $f.cvc4)" >> $f-ref.cpp
+# echo "#include <assert.h> 
+# #include <iostream>
+# #include <sstream>
+# #include <stdlib.h> 
+# #define WIDTH 64" > $f-ref.cpp
+# smt2c "$(tail -n +2 $f.cvc4)" >> $f-ref.cpp
 
-echo "int main(int argc, const char *argv[])
-{
+# echo "int main(int argc, const char *argv[])
+# {
 
-	if(argc!=2 || !(isdigit(*argv[1])))
-	{
-		std::cout<<\"error wrong inputs \" << std::endl;
-		return 1;
-	}
+# 	if(argc!=2 || !(isdigit(*argv[1])))
+# 	{
+# 		std::cout<<\"error wrong inputs \" << std::endl;
+# 		return 1;
+# 	}
 
-	unsigned long int a;
+# 	unsigned long int a;
 
-	// arg 1 is x
-	std::istringstream ssA(argv[1]);
-	if(!(ssA >> a))
-		std::cerr<<\"Unable to parse input value\"<<std::endl;
+# 	// arg 1 is x
+# 	std::istringstream ssA(argv[1]);
+# 	if(!(ssA >> a))
+# 		std::cerr<<\"Unable to parse input value\"<<std::endl;
 
 
-	std::cout<< \"(_ bv\" <<f(a)<< \" \" << WIDTH <<\")\" << std::endl; 
-	return 0;
-}" >> $f-ref.cpp
+# 	std::cout<< \"(_ bv\" <<f(a)<< \" \" << WIDTH <<\")\" << std::endl; 
+# 	return 0;
+# }" >> $f-ref.cpp
 g++ $f-ref.cpp -o $f-ref
 
 
